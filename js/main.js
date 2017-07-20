a2ad33e3f3f8a646b8825f62b8c07ef5a502d4cb
Hou <vivhou@gmail.com>
2017-07-20 14:49:09 -0400

    added help icon

--- js/main.js
+++ js/main.js
@@ -761,16 +761,24 @@ var vizContent = function() {
       .attr("class", "tooltip")
       .style("opacity", 0);
 
-      d3.select(".checkbox-label")
-        .on("mouseover", function() { 
+      d3.select(".help-div")
+        .on("mouseover", function() {
+          d3.select(".help-button")
+            .classed("hide", true)
+          d3.select(".help-button-hover")
+            .classed("hide", false)
           div.transition()
             .duration(200)
-            .style("opacity", .85);
+            .style("opacity", 1);
           div.html("Cost-adjusted numbers account for cost differences among districts.")
             .style("left", (d3.event.pageX) + "px")
-            .style("top", (d3.event.pageY) + 10 + "px");
+            .style("top", (d3.event.pageY) + 10 + "px")
         })
         .on("mouseout", function(d) {
+          d3.select(".help-button")
+            .classed("hide", false)
+          d3.select(".help-button-hover")
+            .classed("hide", true)
           div
             .transition()
             .duration(500)
